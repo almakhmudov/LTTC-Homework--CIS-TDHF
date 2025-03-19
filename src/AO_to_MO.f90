658e5067d5615700b640fe94f68d91b731f797bb
Albert Makhmudov <albrt.makhmudov@gmail.com>
2025-03-19 13:52:26 +0100

    Added the code for the AO to MO transformation as well as the CIS part.

--- src/AO_to_MO.f90
+++ src/AO_to_MO.f90
@@ -1,30 +1,82 @@
-subroutine AO_to_MO(nBas,c,ERI_AO,ERI_MO)
-
-! Expression of bi-electronic integrals in the MO basis set
-
+subroutine AO_to_MO(nBas, c, ERI_AO, ERI_MO)
   implicit none
-!  include 'parameters.h'
 
-! Input variables
-
-  integer,intent(in)            :: nBas
-  double precision,intent(in)   :: c(nBas,nBas)
-  double precision,intent(in)   :: ERI_AO(nBas,nBas,nBas,nBas)
-
-! Local variables
-
-  integer                       :: mu,nu,la,si
-  integer                       :: p,q,r,s
-  double precision,allocatable  :: scr(:,:,:,:)
-
-! Output variables
-
-  double precision,intent(out)  :: ERI_MO(nBas,nBas,nBas,nBas)
-
-! Memory allocation
+  ! Input/Output variables
+  integer, intent(in) :: nBas
+  double precision, intent(in) :: c(nBas,nBas)
+  double precision, intent(in) :: ERI_AO(nBas,nBas,nBas,nBas)
+  double precision, intent(out) :: ERI_MO(nBas,nBas,nBas,nBas)
   
-!--------------------------------------
-! AO to MO transformation starts here !
-!--------------------------------------
+  ! Local variables
+  integer :: mu, nu, la, si
+  integer :: p, q, r, s
+  double precision, allocatable :: tmp1(:,:,:,:), tmp2(:,:,:,:)
+  
+  ! Hello World
+  write(*,*)
+  write(*,*) '**************************************'
+  write(*,*) '|       AO to MO Transformation      |'
+  write(*,*) '**************************************'
+  write(*,*)
+  
+  ! Memory allocation
+  allocate(tmp1(nBas, nBas, nBas, nBas))
+  allocate(tmp2(nBas, nBas, nBas, nBas))
+  
+  ! Initialisation
+  tmp1 = 0.0d0
+  tmp2 = 0.0d0
+  
+  ! First quarter-transformation
+  do p = 1, nBas
+    do mu = 1, nBas
+      tmp1(p,:,:,:) = tmp1(p,:,:,:) + c(mu,p)*ERI_AO(mu,:,:,:)
+    end do
+  end do
+  
+  ! Second quarter-transformation
+  do p = 1, nBas
+    do q = 1, nBas
+      do nu = 1, nBas
+        tmp2(p,q,:,:) = tmp2(p,q,:,:) + c(nu,q)*tmp1(p,nu,:,:)
+      end do
+    end do
+  end do
+  
+  tmp1 = 0.0d0
 
+  ! Third quarter-transformation
+  do p = 1, nBas
+    do q = 1, nBas
+      do r = 1, nBas
+        do la = 1, nBas
+          tmp1(p,q,r,:) = tmp1(p,q,r,:) + c(la,r)*tmp2(p,q,la,:)
+        end do
+      end do
+    end do
+  end do
+  
+  tmp2 = 0.0d0
+
+  ! Fourth quarter-transformation
+  do p = 1, nBas
+    do q = 1, nBas
+      do r = 1, nBas
+        do s = 1, nBas
+          do si = 1, nBas
+            tmp2(p,q,r,s) = tmp2(p,q,r,s) + c(si,s)*tmp1(p,q,r,si)
+          end do
+        end do
+      end do
+    end do
+  end do
+  
+  ERI_MO = tmp2
+  
+  ! Deallocate temporary arrays
+  deallocate(tmp1, tmp2)
+  
+  write(*,*) 'AO to MO transformation completed successfully!'
+  write(*,*)
+  
 end subroutine AO_to_MO
---
+++ src/CIS.f90
@@ -0,0 +1,70 @@
+subroutine CIS(nBas, nO, e, ERI_MO, nCISstates, omega)
+    implicit none
+
+    ! Input/Output variables
+    integer, intent(in) :: nBas              ! Number of basis functions
+    integer, intent(in) :: nO                ! Number of occupied orbitals
+    double precision, intent(in) :: e(nBas)  ! Orbital energies
+    double precision, intent(in) :: ERI_MO(nBas,nBas,nBas,nBas) ! Two-electron integrals in MO basis
+    integer, intent(in) :: nCISstates        ! Number of CIS states to compute
+    double precision, intent(out) :: omega(nCISstates) ! Excitation energies
+    
+    ! Local variables
+    integer :: i, j, a, b, ia, jb
+    integer :: nV, nStates
+    double precision, allocatable :: M(:,:)
+
+    ! Hello World
+    write(*,*)
+    write(*,*) '**************************************'
+    write(*,*) '|          CIS calculation           |'
+    write(*,*) '**************************************'
+    write(*,*)
+    
+    ! Number of virtual orbitals
+    nV = nBas - nO
+    
+    ! Dimension of the CIS matrix
+    nStates = nO * nV
+    
+    ! Allocate memory for the CIS matrix
+    allocate(M(nStates, nStates))
+    
+    ! Construct the CIS matrix
+    M = 0.0d0
+    do ia = 1, nStates
+        i = (ia - 1) / nV + 1
+        a = mod(ia - 1, nV) + nO + 1
+        
+        do jb = 1, nStates
+            j = (jb - 1) / nV + 1
+            b = mod(jb - 1, nV) + nO + 1
+            
+            ! Diagonal term
+            if (i == j .and. a == b) then
+                M(ia,jb) = e(a) - e(i)
+            end if
+            
+            ! Exchange terms
+            M(ia,jb) = M(ia,jb) + 2.0d0 * ERI_MO(i,b,a,j) - ERI_MO(i,b,j,a)
+        end do
+    end do
+    
+    ! Call the diagonalisation subroutine
+    call diagonalize_matrix(nStates, M, omega)
+    
+    ! Print the excitation energies in eV
+    write(*,*) '----------------------------------'
+    write(*,*) '|    Excitation energies (eV):   |'
+    write(*,*) '----------------------------------'
+    
+    do i = 1, nCISstates
+        write(*,'(1X,A1,1X,I5,3X,A1,1X,F20.3,1X,A1)') &
+              '|', i, '|', omega(i)*27.2114d0, '|'
+    end do
+    write(*,*) '----------------------------------'
+    
+    ! Deallocate memory
+    deallocate(M)
+    
+end subroutine CIS
